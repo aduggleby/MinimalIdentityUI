6837723e1e433b4f727ade29e65e4701f2b4523c
Alex Duggleby <151508+aduggleby@users.noreply.github.com>
2024-09-14 10:14:00 +0200

    Add Rate Limiting

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,13 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using MinimalIdentityUI.Data;
+using System.Threading.RateLimiting;
+using RedisRateLimiting.AspNetCore;
+using RedisRateLimiting;
+using StackExchange.Redis;
+using Humanizer.Localisation;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,12 +20,126 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
-var razorPageBuild = builder.Services.AddRazorPages();
+#region Rate Limiting Setup
+
+var useRedisRateLimiter = true;
+
+if (!useRedisRateLimiter)
+{
+    #region Rate Limiting Configuration
+
+    builder.Services.AddRateLimiter(options =>
+    {
+        // Switch to standard 429 response, default is 503
+        options.RejectionStatusCode = 429;
+
+        // Policy for login and register endpoints: rate limited to 10 requests per minute
+        options.AddPolicy(RateLimiterPolicy.LoginAndRegister, httpContext =>
+            RateLimitPartition.GetSlidingWindowLimiter(
+                // Partition key is the IP address of the client (will be overwritten by UseForwardedHeaders below if proxied)
+                partitionKey: $"{RateLimiterPolicy.LoginAndRegister}-{httpContext.Connection.RemoteIpAddress?.ToString()}",
+                factory: _ => new SlidingWindowRateLimiterOptions
+                {
+                    PermitLimit = 10,
+                    Window = TimeSpan.FromMinutes(1)
+                }));
+
+        // Policy for anonymous identity endpoints: rate limited to 5 requests per minute
+        options.AddPolicy(RateLimiterPolicy.ForgotPassword, httpContext =>
+            RateLimitPartition.GetSlidingWindowLimiter(
+                // Partition key is the IP address of the client (will be overwritten by UseForwardedHeaders below if proxied)
+                partitionKey: $"{RateLimiterPolicy.ForgotPassword}-{httpContext.Connection.RemoteIpAddress?.ToString()}",
+                factory: _ => new SlidingWindowRateLimiterOptions
+                {
+                    PermitLimit = 5,
+                    Window = TimeSpan.FromMinutes(1)
+                }));
+
+        // Policy for all other endpoints: rate limited to 100 requests per minute
+        options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(httpContext =>
+            RateLimitPartition.GetSlidingWindowLimiter(
+                partitionKey: httpContext.Connection.RemoteIpAddress?.ToString() ?? string.Empty,
+                factory: partition => new SlidingWindowRateLimiterOptions
+                {
+                    AutoReplenishment = true,
+                    PermitLimit = 100,
+                    Window = TimeSpan.FromMinutes(1),
+                    SegmentsPerWindow = 4
+                }));
+    });
+
+    #endregion
+}
+else
+{
+    #region Rate Limiting Configuration using Redis as a backplane for distributed rate limiting
+
+    // Replace with 
+    var connectionMultiplexer = ConnectionMultiplexer.Connect(
+        builder.Configuration.GetConnectionString("Redis") ??
+            throw new InvalidOperationException("Connection string 'Redis' not found."));
+
+    builder.Services.AddRateLimiter(options =>
+    {
+        // Switch to standard 429 response, default is 503
+        options.RejectionStatusCode = 429;
+
+        // Policy for login and register endpoints: rate limited to 10 requests per minute
+        options.AddPolicy(RateLimiterPolicy.LoginAndRegister, httpContext =>
+            RedisRateLimitPartition.GetSlidingWindowRateLimiter(
+                // Partition key is the IP address of the client (will be overwritten by UseForwardedHeaders below if proxied)
+                partitionKey: $"{RateLimiterPolicy.LoginAndRegister}-{httpContext.Connection.RemoteIpAddress?.ToString()}",
+                factory: _ => new RedisSlidingWindowRateLimiterOptions
+                {
+                    ConnectionMultiplexerFactory = () => connectionMultiplexer,
+                    PermitLimit = 10,
+                    Window = TimeSpan.FromMinutes(1)
+                }));
+
+        // Policy for anonymous identity endpoints: rate limited to 5 requests per minute
+        options.AddPolicy(RateLimiterPolicy.ForgotPassword, httpContext =>
+            RedisRateLimitPartition.GetSlidingWindowRateLimiter(
+                // Partition key is the IP address of the client (will be overwritten by UseForwardedHeaders below if proxied)
+                partitionKey: $"{RateLimiterPolicy.ForgotPassword}-{httpContext.Connection.RemoteIpAddress?.ToString()}",
+                factory: _ => new RedisSlidingWindowRateLimiterOptions
+                {
+                    ConnectionMultiplexerFactory = () => connectionMultiplexer,
+                    PermitLimit = 5,
+                    Window = TimeSpan.FromMinutes(1)
+                }));
+
+        // Policy for all other endpoints: rate limited to 100 requests per minute
+        options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(httpContext =>
+            RedisRateLimitPartition.GetSlidingWindowRateLimiter(
+                partitionKey: httpContext.Connection.RemoteIpAddress?.ToString() ?? string.Empty,
+                factory: partition => new RedisSlidingWindowRateLimiterOptions
+                {
+                    ConnectionMultiplexerFactory = () => connectionMultiplexer,
+                    PermitLimit = 100,
+                    Window = TimeSpan.FromMinutes(1),
+                }));
+
+
+    });
+
+    #endregion
+}
+
+#endregion Rate Limiting Setup
+
+var razorPageBuild = builder.Services.AddRazorPages(options =>
+{
+    options.Conventions.RateLimitAreaPage("Identity", "/Account/Register", RateLimiterPolicy.LoginAndRegister);
+    options.Conventions.RateLimitAreaPage("Identity", "/Account/Login", RateLimiterPolicy.LoginAndRegister);
+    options.Conventions.RateLimitAreaPage("Identity", "/Account/ForgotPassword", RateLimiterPolicy.ForgotPassword);
+});
 
 if (builder.Environment.IsDevelopment())
 {
     razorPageBuild.AddRazorRuntimeCompilation();
 }
+
+
 
 var app = builder.Build();
 
@@ -37,10 +158,27 @@ else
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+
 app.UseRouting();
+
+#region Rate Limiting Activation (after Static Files and after UseRouting)
+
+// Enable overwriting RemoteIpAddress with X-Forwarded-For header for Rate Limiter
+app.UseForwardedHeaders();
+
+app.UseRateLimiter();
+
+#endregion
 
 app.UseAuthorization();
 
 app.MapRazorPages();
 
 app.Run();
+
+
+public static class RateLimiterPolicy
+{
+    public const string LoginAndRegister = "login-and-register";
+    public const string ForgotPassword = "forgot-password";
+};
---
+++ src/RateLimitingPageConventionCollectionExtensions.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.RateLimiting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Humanizer.Localisation;
+
+/// <summary>
+/// Allows setting rate limiting policy on pages, folders and areas similar 
+/// to the Authorize* extension methods.
+/// 
+/// Note this will only work if you endpoint routing is enabled. 
+/// 
+/// Order of pipeline execution is important:
+/// Routing > Rate Limiter > MapRazorPages
+/// 
+/// Example pipeline:
+/// app.UseRouting();
+/// app.UseForwardedHeaders(); // Only necessary if behind a reverse proxy
+/// app.UseRateLimiter();
+/// app.UseAuthorization();
+/// app.MapRazorPages();
+/// 
+/// Example usage:
+/// builder.Services.AddRazorPages(options =>
+/// {
+///    options.Conventions.RateLimitPage("/Page", "policy");
+///    options.Conventions.RateLimitFolder("/Folder", "policy");
+///    options.Conventions.RateLimitAreaPage("Area", "/Page", "policy");
+///    options.Conventions.RateLimitAreaFolder("Area", "/Folder", "policy");
+/// });
+/// </summary>
+public static class RateLimitingPageConventionCollectionExtensions
+{
+    public static PageConventionCollection RateLimitPage(
+		this PageConventionCollection conventions, 
+		string pageName, 
+		string policy)
+    {
+        ArgumentNullException.ThrowIfNull(conventions);
+
+        if (string.IsNullOrEmpty(pageName))
+        {
+            throw new ArgumentException(nameof(pageName));
+        }
+
+        conventions.AddPageApplicationModelConvention(pageName, model =>
+        {
+            model.EndpointMetadata.Add(new EnableRateLimitingAttribute(policy));
+        });
+        return conventions;
+    }
+
+    public static PageConventionCollection RateLimitAreaPage(
+		this PageConventionCollection conventions,
+		string areaName,
+		string pageName,
+		string policy)
+	{
+		ArgumentNullException.ThrowIfNull(conventions);
+
+		if (string.IsNullOrEmpty(areaName))
+		{
+			throw new ArgumentException(nameof(areaName));
+		}
+
+		if (string.IsNullOrEmpty(pageName))
+		{
+			throw new ArgumentException(nameof(pageName));
+		}
+
+		conventions.AddAreaPageApplicationModelConvention(areaName, pageName, model =>
+		{
+			model.EndpointMetadata.Add(new EnableRateLimitingAttribute(policy));
+		});
+		return conventions;
+	}
+	
+	public static PageConventionCollection RateLimitFolder(
+		this PageConventionCollection conventions, 
+		string pageName, 
+		string policy)
+	{
+		ArgumentNullException.ThrowIfNull(conventions);
+
+		if (string.IsNullOrEmpty(pageName))
+		{
+			throw new ArgumentException(nameof(pageName));
+		}
+
+		conventions.AddFolderApplicationModelConvention(pageName, model =>
+		{
+			model.EndpointMetadata.Add(new EnableRateLimitingAttribute(policy));
+		});
+
+		return conventions;
+	}
+
+	public static PageConventionCollection RateLimitAreaFolder(
+		this PageConventionCollection conventions,
+		string areaName,
+		string folderPath,
+		string policy)
+	{
+		ArgumentNullException.ThrowIfNull(conventions);
+
+		if (string.IsNullOrEmpty(areaName))
+		{
+			throw new ArgumentException(nameof(areaName));
+		}
+
+		if (string.IsNullOrEmpty(folderPath))
+		{
+			throw new ArgumentException(nameof(folderPath));
+		}
+
+		conventions.AddAreaFolderApplicationModelConvention(areaName, folderPath, model =>
+		{
+			model.EndpointMetadata.Add(new EnableRateLimitingAttribute(policy));
+		});
+		return conventions;
+	}
+}
+
